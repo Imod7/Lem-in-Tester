6f570ebd76a16d30f960eef4f6b77fa104426604
dominique saripapazoglou <dominiquesaripapazoglou@MacBook-Pro.home>
2020-07-02 19:48:28 +0200

    Changes in script & Generation of maps - The amount of maps generated & the option of deleting maps at the end, now it is based on the user input - Changed script code to compare with generator results but also with verifier script if it is available

--- README.md
+++ README.md
@@ -1,20 +1,18 @@
 ## Prerequisites
 1. Copy in this folder your lem-in executable and the generator script
-2. Go to your .zshrc file (~/.zshrc) at the end of the file and add at the end this line
-```
-. /path/where/you/cloned/the/repo/generate_maps.sh
-```
 
-## Run
-1. Run the initial script :
+## How to run
+1. Run the run script with the following command :
 ```
-$ ./initial.sh
+$ ./run.sh
 ```
-2. Close the opened terminals
-3. Run the run script :
-```
-run.sh
-```
+2. You will be asked to give the number of maps you want to generate per category (flow-one, flow-ten, flow-thousand, big and big-superposition)
+3. All maps are generated and saved in the testmaps folder 
+4. Then each map is tested with your lem-in executable and prints if you passed the test (less or equal lines as the map requires) or failed (more lines than the map requires)
+5. When all maps are tested, you are asked if you wish to keep the generated maps in the testmaps folder or delete them
+6. At the end, the final results are shown at the end when the execution is completed
 
-## Caution
-The generator generates same maps multiple times so make sure to run the script a lot of times.
+
+### A screenshot from the Lem-in-Tester
+![lemin_tester](/images/lemin_test.png)
+
--- generate_maps.sh
+++ generate_maps.sh
@@ -1,21 +1,33 @@
-RANDOM=$$
+printf "Generating maps "
 
-filename="flow-one${RANDOM}"
-echo $filename
-gen="$(./generator --flow-one > testmaps/$filename)"
+counter=1
+while [ $counter -le $1 ]
+do
+	filename="flow-one${counter}"
+	# echo $filename
+	gen="$(./generator --flow-one > testmaps/$filename)"
 
-filename="flow-ten${RANDOM}"
-echo $filename
-gen="$(./generator --flow-ten > testmaps/$filename)"
+	filename="flow-ten${counter}"
+	# echo $filename
+	gen="$(./generator --flow-ten > testmaps/$filename)"
 
-filename="flow-thousand${RANDOM}"
-echo $filename
-gen="$(./generator --flow-thousand > testmaps/$filename)"
+	filename="flow-thousand${counter}"
+	# echo $filename
+	gen="$(./generator --flow-thousand > testmaps/$filename)"
 
-filename="big${RANDOM}"
-echo $filename
-gen="$(./generator --big > testmaps/$filename)"
+	filename="big${counter}"
+	# echo $filename
+	gen="$(./generator --big > testmaps/$filename)"
 
-filename="big-superposition${RANDOM}"
-echo $filename
-gen="$(./generator --big-superposition > testmaps/$filename)"
+	printf "."
+
+	filename="big-superposition${counter}"
+	# echo $filename
+	gen="$(./generator --big-superposition > testmaps/$filename)"
+
+	printf "."
+
+  	((counter++))
+done
+
+printf "\nAll maps were generated successfully\n"
